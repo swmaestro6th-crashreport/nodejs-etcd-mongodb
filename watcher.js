a90c4fcc7c19a8565f8058a543082e374bc22180
Eun Hwan Park <topbladep@gmail.com>
2015-08-11 19:22:15 +0900

    Watcher 코드 일부 수정

--- watcher.js
+++ watcher.js
@@ -1,16 +1,19 @@
 var Etcd = require('node-etcd'),
     exec = require('child_process').exec,
+    sys = require('sys'),
     config = require('./config');
 
 var etcd = new Etcd();
 
 var api = {
     watch: function (key) {
-        etcd.set(config.etcd.key1, config.mongod1, function set() {
-            exec(config.mongod1, function (err, stdout, stderr) {
-                setTimeout(set, 10);
-            });
-        });
+      etcd.get(config.etcd.key1, function(err, value) {
+          etcd.set(config.etcd.key1, config.mongod1, function set() {
+              exec(config.mongod1, function (err, stdout, stderr) {
+                setTimeout(set, 1);
+              })
+          })
+      })
     }
 }
 
