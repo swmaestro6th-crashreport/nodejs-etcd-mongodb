5e048bf6180b6469f63948aeeb4f22454ec9920a
andmylife <topbladep@gmail.com>
2015-08-18 07:10:07 +0900

    update mongod setting

--- app.js
+++ app.js
@@ -2,18 +2,24 @@ var api = require('./connect'),
 	Etcd = require('node-etcd'),
     config = require('./config');
 
-console.log('wathcer start');
+console.log('Etcd : Server started');
 
 var etcd = new Etcd();
 
-api.connect(etcd, config.etcd.master, function () {
-	api.notify(etcd, config.etcd.master, function () {
-		api.setup(etcd, config.etcd.master)
+api.connect(etcd, config.etcd.replSet1, function () {
+	api.notify(etcd, config.etcd.replSet1, function () {
+		api.setup(etcd, config.etcd.replSet1)
 	})
 });
 
-api.connect(etcd, config.etcd.slave, function () {
-	api.notify(etcd, config.etcd.slave, function () {
-		api.setup(etcd, config.etcd.slave)
+api.connect(etcd, config.etcd.replSet2, function () {
+	api.notify(etcd, config.etcd.replSet2, function () {
+		api.setup(etcd, config.etcd.replSet2)
+	});
+});
+
+api.connect(etcd, config.etcd.replSet3, function () {
+	api.notify(etcd, config.etcd.replSet3, function () {
+		api.setup(etcd, config.etcd.replSet3)
 	});
 });
--- watcher.js
+++ watcher.js
@@ -4,13 +4,16 @@ var Etcd = require('node-etcd'),
 
 var etcd = new Etcd();
 
+console.log('Etcd : Watcher started');
+
 var watcher = {};
-watcher.machine1 = etcd.watcher(config.etcd.master.key);
-watcher.machine2 = etcd.watcher(config.etcd.slave.key);
+watcher.machine1 = etcd.watcher(config.etcd.replSet1.key);
+watcher.machine2 = etcd.watcher(config.etcd.replSet2.key);
+watcher.machine3 = etcd.watcher(config.etcd.replSet3.key);
 
 watcher.machine1.on('delete', function set (data) {
 	console.log("delete : " + data);
-	exec(config.etcd.master.mongod, function (err, stdout, stderr) {
+	exec(config.etcd.replSet1.mongod, function (err, stdout, stderr) {
 		console.log('etcd -> reloaded mongodb server port : 20000')
 		setTimeout(set, 1);
 	});
@@ -18,8 +21,16 @@ watcher.machine1.on('delete', function set (data) {
 
 watcher.machine2.on('delete', function set (data) {
 	console.log("delete : " + data);
-	exec(config.etcd.slave.mongod, function (err, stdout, stderr) {
+	exec(config.etcd.replSet2.mongod, function (err, stdout, stderr) {
 		console.log('etcd -> reloaded mongodb server port : 30000')
+		setTimeout(set, 1);
+	});
+});	
+
+watcher.machine3.on('delete', function set (data) {
+	console.log("delete : " + data);
+	exec(config.etcd.replSet3.mongod, function (err, stdout, stderr) {
+		console.log('etcd -> reloaded mongodb server port : 40000')
 		setTimeout(set, 1);
 	});
 });	
